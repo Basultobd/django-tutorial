68a9468c23c66e12e353f732661faa6eebf2e85a
d.basulto <basultobd@gmail.com>
2018-11-15 14:42:47 -0600

    add function for verify if question was recently posted

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
@@ -11,6 +14,11 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        # print(self.pub_date)
+        # print(timezone.now())
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
 
