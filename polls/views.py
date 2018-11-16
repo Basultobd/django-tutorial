2b97ee2a8d926698f7e05d6fd7c4d47f6a2e1b15
d.basulto <basultobd@gmail.com>
2018-11-16 16:52:56 -0600

    add not found error handler

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,30 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
+
+# from django.http import HttpResponse
+# from django.template import loader
+
 from .models import Question
 
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    # template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', { 'question': question })
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404('Question with id %s does not exists' % question_id)
+    # return render(request, 'polls/detail.html', { 'question': question })
+    # # return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
