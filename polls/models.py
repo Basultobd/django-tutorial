199ee7004f4f9377fc996f267b1ae4b5443a361f
d.basulto <basultobd@gmail.com>
2018-11-16 16:51:42 -0600

    format meta class place

--- polls/models.py
+++ polls/models.py
@@ -6,11 +6,11 @@ from django.utils import timezone
 # Create your models here.
 class Question(models.Model):
 
-    class Meta:
-        db_table = 'question'
-
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    class Meta:
+        db_table = 'question'
 
     def __str__(self):
         return self.question_text
@@ -22,13 +22,13 @@ class Question(models.Model):
 
 class Choice(models.Model):
 
-    class Meta:
-        db_table = 'choice'
-
     # each choice is associated with a question
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes_count = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'choice'
 
     def __str__(self):
         return self.choice_text
